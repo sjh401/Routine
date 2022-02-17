55ece662dc27b598eee4c0887a99243420ecbe52
stephenharrity <sjh401@gmail.com>
2022-02-17 13:23:29 -0600

    app set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
+import { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Layout from "./components/layout/Layout";
+const Home = lazy(() => import('./screens/Home'));
 
 
-function App() {
-  return (
-    <div className="App">
-      <Layout>
-        
-      </Layout>
-    </div>
-  );
-}
+const App = () => (
+    <Layout>
+      <Router>
+        <Suspense fallback={<div>...loading</div>}>
+          <Routes>
+            <Route path='/' element={<Home/>}/>
+          </Routes>
+          </Suspense>
+      </Router>
+    </Layout>
+)
 
 export default App;
---
+++ client/src/container/Container.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function Container() {
+  return(
+      <div> welcome</div>
+
+  ) 
+}
