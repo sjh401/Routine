605ca5591e03a767c389842f929681b39b4f5f8b
stephenharrity <sjh401@gmail.com>
2022-02-25 17:34:49 -0600

    routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,16 @@
-import { lazy, Suspense } from "react";
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import Container from "./container/Container";
-import Calendar from "./screens/Calendar";
-import Home from "./screens/Home";
 import Login from "./screens/Login"
 import Register from "./screens/Register";
-// const Home = lazy(() => import('./screens/Home'));
+
 
 
 const App = () => {
-  
+
+  const handleRegister = () => {
+    
+  }
   return (
     <Layout>
       <Routes>
--- client/src/container/Container.jsx
+++ client/src/container/Container.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { Navigate, Outlet, Route, Routes } from 'react-router-dom';
-import App from '../App';
+import { Route, Routes } from 'react-router-dom';
 import Calendar from '../screens/Calendar';
 import Home from '../screens/Home';
-import { Component } from 'react';
+
 
 export default function Container() {
     return(
