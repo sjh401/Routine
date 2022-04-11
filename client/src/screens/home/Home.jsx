4cfa910c5cad0624fda25477d8ceef09d723974d
stephenharrity <sjh401@gmail.com>
2022-04-11 12:46:17 -0500

    staring home styling

--- client/src/screens/home/Home.jsx
+++ client/src/screens/home/Home.jsx
@@ -5,6 +5,9 @@ import './Home.css'
 export default function Home(props) {
   const { currentUser, allItems } = props;
 
+  const today = new Date();
+  console.log(allItems[0]?.to_do_date)
+  console.log(today.getDate())
   return (
   <React.Fragment>
       <div className='flex-row-wrap'>
@@ -16,23 +19,63 @@ export default function Home(props) {
         </h4>
       </div>
       <div className='flex-row-wrap'>
-        {allItems.length && allItems.map(item => {
-          return (
-            <div 
+        <div className='flex-column'>
+          <h3>
+            Today's To-dos
+          </h3>
+          <div>
+            {allItems.length && allItems.filter(item => {
+              if(Number(item.to_do_date.substring(8,10)) === today.getDate()){
+                return item
+              }
+              }).map(item => {
+              return (
+                <div 
                 key={`${item.id} ${item.title}`}
                 className='flex-column'
-              >
-            <Link to={`/item/${item.id}`}>
-                <h4>
-                  {item.title}
-                </h4>
-            </Link>
-                <div>
-                  {item.completed === true ? 'complete' : 'not'}
-                </div>
-              </div>
-          )
-        })}
+                >
+                <Link to={`/item/${item.id}`}>
+                    <h4>
+                      {item.title}
+                    </h4>
+                </Link>
+                    <div>
+                      {item.completed === true ? 'complete' : 'not'}
+                    </div>
+                  </div>
+              )
+            })}
+          </div>
+        </div>
+        <div>
+          <h3>
+            Upcoming
+          </h3>
+          <div>
+          
+            {allItems.length && allItems.filter(item => {
+              if(Number(item.to_do_date.substring(8,10)) > today.getDate() && Number(item.to_do_date.substring(6,8)) > today.getMonth() + 1){
+                return item
+              }
+              }).map(item => {
+                return (
+                  <div 
+                  key={`${item.id} ${item.title}`}
+                  className='flex-column'
+                  >
+                  <Link to={`/item/${item.id}`}>
+                      <h4>
+                        {item.title}
+                      </h4>
+                  </Link>
+                      <div>
+                        {item.completed === true ? 'complete' : 'not'}
+                      </div>
+                    </div>
+                )
+            })}
+          </div>
+        </div>
       </div>
   </React.Fragment>
   ) 
